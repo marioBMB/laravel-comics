a6fe243ea9b9cc6998bdfe1561d56c37e8386609
marioBMB <m.sannicola@studenti.uniba.it>
2022-03-04 16:55:16 +0100

    modifiche varie

--- resources/views/commons/banner-icon.blade.php
+++ resources/views/commons/banner-icon.blade.php
@@ -1,7 +1,7 @@
 
 
 <div class="banner-icon">
-    <img src="{{asset('img/'+$banner['thumb'])}}" alt="{{$banner['category']}}">
+    <img src="{{asset('img/'.$banner['thumb'])}}" alt="{{$banner['category']}}">
     <h4>
         {{$banner['category']}}
     </h4>
